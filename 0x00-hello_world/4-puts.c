643030d8b4247c7381531e4b1a809d40a432283a
Locozyoungg <collaustin27@gmail.com>
2023-09-07 08:10:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo '#include <stdio.h>' > program.c && echo 'int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }' >> program.c && betty program.c && ./program && rm program program.c
+echo '#include <stdio.h>' > program.c && echo 'int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }' >> program.c && betty program.c && ./program
