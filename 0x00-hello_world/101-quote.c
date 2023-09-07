1945fd3ee121f6fc16161e0b242f71a023ec3c3b
Locozyoungg <collaustin27@gmail.com>
2023-09-07 12:26:17 -0700

    8 commit1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the specified message to the standard error.
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
+return (1);
+}
