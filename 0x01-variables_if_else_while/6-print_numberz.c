8c0f540d8a7c0a1f8ec39ec67eb592315712a9e1
Locozyoungg <collaustin27@gmail.com>
2023-09-10 02:23:07 -0700

    description

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+* main - Entry point of the program
+*
+* Description:
+* This program prints all single-digit numbers in base 10, starting from 0 to 9,
+* followed by a new line. It adheres to the specified requirements:
+* - It uses the `write` function to print the numbers with only two `putchar`-like calls.
+* - It does not use any variables of type `char`.
+* - It includes a newline character ('\n') at the end to create a new line.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
 if (write(1, "0123456789\n", 11) == -1)
