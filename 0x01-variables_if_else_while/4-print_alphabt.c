c59afee4978ddfc079a331015a481ff4ec753eba
Locozyoungg <collaustin27@gmail.com>
2023-09-10 00:47:06 -0700

    documentation

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+* main - Entry point of the program
+*
+* Description:
+* This function prints the lowercase alphabet, excluding the letters 'e' and 'q',
+* followed by a newline character. It uses the putchar function to print.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
 char letter = 'a';
