a8d42e409c5b8d79eff3fa4ab6e399b3a666101a
Locozyoungg <collaustin27@gmail.com>
2023-09-10 04:00:47 -0700

    comment-fix

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Description:
+* The functn prints all possible combos of two two-dgt numbrs in ascending order,
+* It ensures that the two numbers are distinct.
+* Numbers are printed with two digits, and the combo is separated by a comma.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int num1, num2;
+
+for (num1 = 0; num1 <= 98; num1++)
+{
+for (num2 = num1 + 1; num2 <= 99; num2++)
+{
+putchar('0' + (num1 / 10));
+putchar('0' + (num1 % 10));
+putchar(' ');
+putchar('0' + (num2 / 10));
+putchar('0' + (num2 % 10));
+
+if (num1 != 98 || num2 != 99)
+            {
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
