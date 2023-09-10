576101ea99ca6f6274fdbff8954363eda38d8232
Locozyoungg <collaustin27@gmail.com>
2023-09-10 03:36:20 -0700

    description-fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-
+/**
+* main - Entry point of the program
+*
+* Description:
+* The functn prints all possible different combos of two digts in ascending order,
+* It ensures that the two digits are different.
+* Numbers are printed as two-digit strings.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
 int tens_digit, ones_digit;
