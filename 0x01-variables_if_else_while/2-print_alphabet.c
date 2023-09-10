24781fca162d804a603490c96bffdf1da3defa4c
Locozyoungg <collaustin27@gmail.com>
2023-09-09 23:54:32 -0700

    comment-added

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+* main - Entry point of the program
+*
+* Description:
+* This function prints the lowercase alphabet followed by a newline character.
+* It uses the putchar function to print each character.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
 char letter = 'a';
