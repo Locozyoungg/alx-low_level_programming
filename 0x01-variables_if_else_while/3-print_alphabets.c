762769504ecdce447d92bc838d430e1198f50243
Locozyoungg <collaustin27@gmail.com>
2023-09-10 00:12:37 -0700

    3-print_alphabets-changes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
-/**
-* main - Entry point of the program
-*
-* Description:
-* This function prints the lowercase alphabet, followed by the uppercase alphabet,
-* both followed by a newline character. It uses the putchar function to print each character.
-*
-* Return: 0 (Success)
-*/
+
 int main(void)
 {
 char lowercase = 'a';
