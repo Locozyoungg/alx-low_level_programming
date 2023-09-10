5ff1190d23dad7b263527795a1e22d54d6296779
Locozyoungg <collaustin27@gmail.com>
2023-09-10 01:22:18 -0700

    description

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This function prints all single-digit numbers in base 10, start frm 0 to 9,
+ * followed by a newline character. It uses the putchar function to print.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 int number = 0;
 
 while (number <= 9)
 {
+// Use putchar to print the current digit by adding '0' to it
 putchar(number + '0');
 number++;
 }
