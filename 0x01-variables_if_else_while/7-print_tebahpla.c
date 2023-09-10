1fbbd57eb30f75da7c38f2a17db1589cfe54d2bc
Locozyoungg <collaustin27@gmail.com>
2023-09-10 02:40:51 -0700

    comments

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+* main - Entry point of the program
+*
+* Description:
+* This function prints the lowercase alphabet in reverse order,
+* It uses the putchar function to print each character.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
 char letter = 'z';
