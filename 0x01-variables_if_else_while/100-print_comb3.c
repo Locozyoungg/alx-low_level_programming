88c551e50bf5071d1345c087195a43b488a99dc8
Locozyoungg <collaustin27@gmail.com>
2023-09-10 03:26:55 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+int tens_digit, ones_digit;
+
+for (tens_digit = 0; tens_digit <= 8; tens_digit++)
+{
+for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+{
+putchar('0' + tens_digit);
+putchar('0' + ones_digit);
+
+if (tens_digit != 8 || ones_digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
