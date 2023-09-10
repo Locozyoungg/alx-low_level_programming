4576200bbc4c735aec7fbe6ea3c4706e3bebb2b5
Locozyoungg <collaustin27@gmail.com>
2023-09-10 02:56:38 -0700

    comments

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+* main - Entry point of the program
+*
+* Description:
+* This function prints all the numbers of base 16 (hexadecimal) in lowercase,
+* It uses the putchar function to print each digit.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
 char digit;
