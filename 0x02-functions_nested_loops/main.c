b9746f72923ea3e7cab143f2e63156c3c4c66e9e
Locozyoungg <collaustin27@gmail.com>
2023-09-13 13:45:45 -0700

    last-digit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -107,3 +107,18 @@ return (-n);
 else
 return (n);
 }
+
+/**
+* print_last_digit - Prints the last digit of a number.
+* @n: The number to extract the last digit from.
+*
+* Return: The last digit of n.
+*/
+int print_last_digit(int n)
+{
+int last_digit;
+
+last_digit = _abs(n) % 10;
+_putchar('0' + last_digit);
+return (last_digit);
+}
