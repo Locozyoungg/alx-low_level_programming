7174d64b4005b1e054a8e84bf291717c716fcc20
Locozyoungg <collaustin27@gmail.com>
2023-09-13 13:17:46 -0700

    genchanges

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -54,3 +54,17 @@ return (1);
 else
 return (0);
 }
+
+/**
+* _isalpha - Checks if a character is alphabetic (a letter).
+* @c: The character to check.
+*
+* Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise.
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
