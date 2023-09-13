4addc1f413b595e2657c5c3e76313e23574f2284
Locozyoungg <collaustin27@gmail.com>
2023-09-13 14:47:37 -0700

    time_table

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -223,3 +223,18 @@ printf("\n");
 }
 }
 }
+
+/**
+* add - Adds two integers and returns the result.
+* @a: The first integer.
+* @b: The second integer.
+*
+* Return: The sum of a and b.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* print_to_98 -
