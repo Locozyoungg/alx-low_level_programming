bbe0190456fc7623bf441b3634abe28178a1a0ee
Locozyoungg <collaustin27@gmail.com>
2023-09-13 03:13:14 -0700

    alphafix2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,13 @@
-#include "main_putchar.h"
+#include <unistd.h>
+
 /**
-* main - Entry point
+* _putchar - Writes a character to stdout
+* @c: The character to print
 *
-* Return: Always 0 (Success)
+* Return: On success, 1. On error, -1.
 */
-
-int main(void)
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
+    return write(1, &c, 1);
 }
-
-
 
