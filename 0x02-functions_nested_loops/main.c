82cdf9ce60b62c7a15ad10400786139261dc9e92
Locozyoungg <collaustin27@gmail.com>
2023-09-13 12:46:49 -0700

    alpha2fix

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -11,3 +11,32 @@ print_alphabet();
 return (0);
 }
 
+/**
+* print_alphabet - Prints the alphabet in lowercase followed by a new line.
+*/
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+
+_putchar('\n');
+}
+
+/**
+* print_alphabet_x10 - Prints the alphabet in lowercase 10 times,
+* followed by a new line each time.
+*/
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+print_alphabet();
+}
+}
