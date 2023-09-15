66459e77e61796ff53ce1ad459d51b6650e0de81
Locozyoungg <collaustin27@gmail.com>
2023-09-14 19:11:20 -0700

    fizz_buzz2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* main - prints the numbers from 1 to 100, followed by a new line
-* but for multiples of three prints Fizz instead of the number
-* and for the multiples of five prints Buzz
+* main - prints numbers from 1 to 100 with FizzBuzz logic
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -14,47 +13,28 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-_putchar('F');
-_putchar('i');
-_putchar('z');
-_putchar('z');
+printf("Fizz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-_putchar('B');
-_putchar('u');
-_putchar('z');
-_putchar('z');
+printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-_putchar('F');
-_putchar('i');
-_putchar('z');
-_putchar('z');
-_putchar('B');
-_putchar('u');
-_putchar('z');
-_putchar('z');
+printf("FizzBuzz");
 }
 else
 {
-int num = i;
-
-if (num >= 10)
-{
-_putchar((num / 10) + '0');
-}
-_putchar((num % 10) + '0');
+printf("%d", i);
 }
 
 if (i < 100)
 {
-_putchar(' ');
+printf(" ");
 }
 else
 {
-_putchar('\n');
+printf("\n");
 }
 }
 
