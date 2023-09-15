f39d87bb5b9b04801c71379f7c43f81746e15563
Locozyoungg <collaustin27@gmail.com>
2023-09-14 18:02:40 -0700

    buzzfizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,45 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
 * fizz_buzz - prints numbers from 1 to 100 with FizzBuzz logic
 */
-int main(void)
+void fizz_buzz(void)
 {
 int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 != 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
-printf(" Fizz");
-} else if (i % 5 == 0 && i % 3 != 0)
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
 {
-printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+printf("Fizz");
+}
+else if (i % 5 == 0)
 {
-printf(" FizzBuzz");
-} else if (i == 1)
-{
+printf("Buzz");
+}
+else
 printf("%d", i);
-} else
-{
-printf(" %d", i);
 }
-}
-printf("\n");
 
+if (i < 100)
+{
+printf(" ");
+}
+else
+{
+printf("\n");
+}
+}
+}
+
+int main(void)
+{
+fizz_buzz();
 return (0);
 }
+
