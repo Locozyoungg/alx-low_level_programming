6fb31f60a96c93355f4868a140000c5d853b915f
Locozyoungg <collaustin27@gmail.com>
2023-09-21 15:08:27 -0700

    grandfx

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
-* print_number - prints an integer;
-* @n: integer to be printed;
+* print_number - Prints an integer using _putchar
+* @n: The integer to print
 */
 void print_number(int n)
 {
-unsigned int n1;
-
 if (n < 0)
 {
-n1 = -n;
 _putchar('-');
-} else
-{
-n1 = n;
+n = -n;
 }
 
-if (n1 / 10)
-print_number(n1 / 10);
+if (n / 10 != 0)
+print_number(n / 10);
 
-_putchar((n1 % 10) + '0');
+_putchar((n % 10) + '0');
 }
+
