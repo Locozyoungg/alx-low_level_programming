708c6377c74e2e393b60d8fcf598580f07040b43
Locozyoungg <collaustin27@gmail.com>
2023-09-25 14:43:27 -0700

    memset-fix_2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _memset - fills memory with a constant byte
-* @s: the memory area to fill
+* *_memset - fills memory with a constant byte
+* @s: memory area to be filled
 * @b: char to copy
-* @n: the number oftimes to copy b
+* @n: number of times to copy b
 *
 * Return: pointer to the memory area s
 */
@@ -17,6 +17,5 @@ for (i = 0; i < n; i++)
 s[i] = b;
 }
 
-return s;
+return (s);
 }
-
