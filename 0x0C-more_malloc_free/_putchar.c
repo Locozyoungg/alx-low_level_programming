3bd51c5d4111d25337b3391066f7a0af7a663474
Locozyoungg <collaustin27@gmail.com>
2023-10-05 16:34:41 -0700

    multi

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* _putchar - Writes a character to the standard output (stdout)
+* @c: The character to be written
+*
+* Return: On success, returns the character written as an unsigned char
+*         cast to an int. On error, -1 is returned.
+*/
+int _putchar(char c)
+{
+return fputc(c, stdout);
+}
