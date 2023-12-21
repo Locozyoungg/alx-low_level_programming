de7ac0feba1703cfdc689ed05eee4363715995d9
Locozyoungg <collaustin27@gmail.com>
2023-12-21 14:07:56 -0800

    key_indx

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of a key
+ * @key: The key
+ * @size: The size of the array of the hash table
+ * Return: The index at which the key/value pair should be stored in the array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int hash;
+
+    /* Use the hash_djb2 function to get the hash value of the key */
+    hash = hash_djb2(key);
+
+    /* Use the modulo operator to get the index within the range of the array size */
+    return (hash % size);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,6 +5,14 @@
 #include <string.h>
 #include <stdio.h>
 
+/* Macro definitions */
+
+#define HASH_TABLE_INIT_SIZE 1024 /* Initial size of the hash table array */
+#define HASH_TABLE_LOAD_FACTOR 0.75 /* Maximum load factor before resizing the hash table */
+#define HASH_TABLE_GROWTH_FACTOR 2 /* Factor by which the hash table size is increased */
+
+
+
 /* Data structures */
 
 /**
@@ -36,9 +44,59 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 
+/* Enumerated types */
+
+/* Status codes for hash table operations */
+typedef enum hash_table_status
+{
+    HASH_TABLE_OK, /* Operation successful */
+    HASH_TABLE_FAIL, /* Operation failed */
+    HASH_TABLE_FULL, /* Hash table is full */
+    HASH_TABLE_EMPTY, /* Hash table is empty */
+    HASH_TABLE_KEY_EXISTS, /* Key already exists in hash table */
+    HASH_TABLE_KEY_NOT_FOUND /* Key not found in hash table */
+} hash_table_status_t;
+
+/* Operation codes for hash table functions */
+typedef enum hash_table_operation
+{
+    HASH_TABLE_GET, /* Get the value associated with a key */
+    HASH_TABLE_SET, /* Set or update the value associated with a key */
+    HASH_TABLE_DELETE, /* Delete a key/value pair from the hash table */
+    HASH_TABLE_CLEAR, /* Clear the entire hash table */
+    HASH_TABLE_RESIZE /* Resize the hash table array */
+} hash_table_operation_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_table_status_t hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+hash_table_status_t hash_table_delete(hash_table_t *ht, const char *key);
+hash_table_status_t hash_table_clear(hash_table_t *ht);
+hash_table_status_t hash_table_resize(hash_table_t *ht, unsigned long int new_size);
+void hash_table_print(const hash_table_t *ht);
+
+/* Inline functions */
+
+/**
+ * hash_table_is_empty - Checks if a hash table is empty
+ * @ht: The hash table
+ * Return: 1 if empty, 0 otherwise
+ */
+static inline int hash_table_is_empty(const hash_table_t *ht)
+{
+    return (ht == NULL || ht->size == 0 || ht->array == NULL);
+}
+
+/**
+ * hash_table_size - Returns the size of a hash table
+ * @ht: The hash table
+ * Return: The size of the array, or 0 if empty
+ */
+static inline unsigned long int hash_table_size(const hash_table_t *ht)
+{
+    return (hash_table_is_empty(ht) ? 0 : ht->size);
+}
 
 #endif /* HASH_TABLES_H */
